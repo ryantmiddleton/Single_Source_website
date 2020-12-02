f5eb3987690e1a7450256c959ca0395f6fa29cde
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-12-01 20:48:06 -0800

    created app

--- Ryan_Middleton_Solo_Project/settings.py
+++ Ryan_Middleton_Solo_Project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&v43il6dc1q4=vr=(f7^j5rswm7m$15ijtjk6+uoqtvv5gm_(6'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'single_source_site',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ single_source_site/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SingleSourceSiteConfig(AppConfig):
+    name = 'single_source_site'
