1a5ea1ed8ebe1b7f179e69ddc583a09df6768205
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-12-11 00:53:40 -0800

    finished most of the main functionality

--- single_source_site/views.py
+++ single_source_site/views.py
@@ -9,7 +9,7 @@ def index (request):
 
 def build_quote(request):
     context={
-        'products':Product.objects.filter(category=2)
+        'products':Product.objects.all()
     }
     return render(request, "quote_builder.html", context)
 
@@ -47,11 +47,48 @@ def display_cart(request):
         return render(request, "cart.html", context)
     return redirect("/quote_page")
 
+def get_order(request, order_id):
+    context = {
+        'order' : Order.objects.get(id=order_id)
+    }
+    return render(request, "cart.html", context)
+
+def delete_order_product(request, order_id, product_id):
+    # Get references to the order and product
+    update_order = Order.objects.get(id=order_id)
+    delete_product = Product.objects.get(id=product_id)
+    #Remove the product fromt he order
+    update_order.products.remove(delete_product)
+
+    #Update any/all of the products in the order with  quantities from the form
+    for key in request.POST:
+        if "quantity" in key and int(request.POST[key]) > 0:
+            # parse the product id out of the name of the 'number' element from the form 
+            product_in_order_id = key.replace("_quantity", "")
+            product_in_order = Product.objects.get(id=int(product_in_order_id))
+            # value of the 'number' element is the quantity ordered
+            # Update the order quantity of this product for the order
+            product_in_order.quantity_in_order = request.POST[key]
+            product_in_order.save()
+
+    return redirect("/get_order/"+ str(order_id))
+
 def send_quote(request, order_id):
     if request.method == "POST":
         send_order = Order.objects.get(id=order_id)
+        # First update the order with any edits the user may have made
+        for key in request.POST:
+            if "quantity" in key and int(request.POST[key]) > 0:
+                # parse the product id out of the name of the 'number' element from the form 
+                product_in_order_id = key.replace("_quantity", "")
+                product_in_order = Product.objects.get(id=int(product_in_order_id))
+                # value of the 'number' element is the quantity ordered
+                # Update the order quantity of this product for the order
+                product_in_order.quantity_in_order = request.POST[key]
+                product_in_order.save()
+
+        #Build the html string to put into the email
         email_message = render_to_string('email.html',{'order': send_order})
-        print(email_message)
         send_mail(
             'Order# ' + str(send_order.id),
             'Hi ' + send_order.customer_name + ', \nThank-you for your inquiry. Please see your quote below.',
@@ -60,4 +97,4 @@ def send_quote(request, order_id):
             fail_silently=False,
             html_message = email_message
         )
-    return render (request, "email.html", {'order': send_order})
+    return render (request, "order_success.html", {'order': send_order})
