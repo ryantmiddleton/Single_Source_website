576b0d3beade2882bc9f41f56059836bc35535f0
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-12-14 14:32:36 -0800

    edited settings.py

--- Ryan_Middleton_Solo_Project/settings.py
+++ Ryan_Middleton_Solo_Project/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "single_source_site",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hello",
 ]
 
 MIDDLEWARE = [
