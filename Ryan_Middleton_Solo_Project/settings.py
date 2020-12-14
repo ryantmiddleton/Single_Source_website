67e87ad73b7f397410f072fdf538193e3634f731
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-12-14 09:13:49 -0800

    changed SECRET KEY varable

--- Ryan_Middleton_Solo_Project/settings.py
+++ Ryan_Middleton_Solo_Project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
-
+#SECRET_KEY = secret_key
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
