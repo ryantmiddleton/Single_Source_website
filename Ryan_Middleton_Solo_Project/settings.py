9640e28bc64a35ba1dfbbf20f6f1425601995af5
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-12-16 08:18:33 -0800

    changed settings

--- Ryan_Middleton_Solo_Project/settings.py
+++ Ryan_Middleton_Solo_Project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&v43il6dc1q4=vr=(f7^j5rswm7m$15ijtjk6+uoqtvv5gm_(6')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
