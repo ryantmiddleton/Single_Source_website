b2a36f2ef6d27baf819a020ccae7dd03f914d770
Ryan Middleton <ryanTmiddleton@gmail.com>
2021-02-11 12:59:59 -0800

    added sendgrid dyno

--- Ryan_Middleton_Solo_Project/settings.py
+++ Ryan_Middleton_Solo_Project/settings.py
@@ -35,6 +35,13 @@ ALLOWED_HOSTS = ['*']
 # EMAIL_HOST = 'localhost'
 # EMAIL_PORT = 1025
 
+# SendGric SMTP settings for email service on Heroku
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME', '') # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', '')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Mailgun SMTP settings for email service on Heroku
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
@@ -44,14 +51,14 @@ ALLOWED_HOSTS = ['*']
 # EMAIL_USE_TLS = True
 
 #Mailgun API settings for email service on Heroku
-ANYMAIL = {
-    # (exact settings here depend on your ESP...)
-    "MAILGUN_API_KEY": os.environ.get('MAILGUN_API_KEY', ''),
-    "MAILGUN_SENDER_DOMAIN": os.environ.get('MAILGUN_DOMAIN', '')  # your Mailgun domain, if needed
-}
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or...
-DEFAULT_FROM_EMAIL = os.environ.get('MAILGUN_SMTP_LOGIN', '') # if you don't already have this in settings
-SERVER_EMAIL = os.environ.get('MAILGUN_SMTP_LOGIN', '') # ditto (default from-email for Django errors)
+# ANYMAIL = {
+#     # (exact settings here depend on your ESP...)
+#     "MAILGUN_API_KEY": os.environ.get('MAILGUN_API_KEY', ''),
+#     "MAILGUN_SENDER_DOMAIN": os.environ.get('MAILGUN_DOMAIN', '')  # your Mailgun domain, if needed
+# }
+# EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or...
+# DEFAULT_FROM_EMAIL = os.environ.get('MAILGUN_SMTP_LOGIN', '') # if you don't already have this in settings
+# SERVER_EMAIL = os.environ.get('MAILGUN_SMTP_LOGIN', '') # ditto (default from-email for Django errors)
 
 # Application definition
 INSTALLED_APPS = [
