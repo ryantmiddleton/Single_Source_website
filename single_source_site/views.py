4fc6b5dd7e430c265a93f7abaf93cea6de44be4e
Ryan Middleton <ryanTmiddleton@gmail.com>
2021-02-12 23:09:52 -0800

    formatting email and order success page

--- single_source_site/views.py
+++ single_source_site/views.py
@@ -164,7 +164,8 @@ def send_quote(request, order_id):
         email_message = render_to_string('email.html', context)
         send_mail(
             'Single Source Order# ' + str(send_order.id),
-            render_to_string('Hi ' + str(send_order.customer_name) + ', \nThank-you for your inquiry. Please see your quote below.\n'),
+            message = 'Hi , \nThank-you for your inquiry. Please see your quote below.\n',
+            # 'Hi ' + str(send_order.customer_name) + ', \nThank-you for your inquiry. Please see your quote below.\n'),
             'ryan@single-source.com',
             [send_order.email, 'ryantmiddleton@gmail.com'],
             fail_silently=False,
